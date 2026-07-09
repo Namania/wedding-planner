ce4694819bae9988f2da33a4f70504b9433fbe2e
namania <mael.namania@gmail.com>
2026-07-09 19:29:01 +0200

    fix: allowed hosts

--- resources/vite.config.ts
+++ resources/vite.config.ts
@@ -1,36 +1,44 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
 import Components from 'unplugin-vue-components/vite'
 import { PrimeVueResolver } from 'unplugin-vue-components/resolvers'
 
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-    tailwindcss(),
-    Components({
-      resolvers: [
-        PrimeVueResolver()
+export default defineConfig(({ mode }) => {
+
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [
+      vue(),
+      vueDevTools(),
+      tailwindcss(),
+      Components({
+        resolvers: [
+          PrimeVueResolver()
+        ]
+      }),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    },
+    server: {
+      host: '0.0.0.0',
+      port: 5173,
+      watch: {
+        usePolling: true,
+      },
+      hmr: {
+        host: 'localhost',
+      },
+      allowedHosts: [
+        env.VITE_ALLOWED_HOST || 'localhost'
       ]
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
-  server: {
-    host: '0.0.0.0',
-    port: 5173,
-    watch: {
-      usePolling: true,
-    },
-    hmr: {
-      host: 'localhost',
     }
   }
-})
+});
